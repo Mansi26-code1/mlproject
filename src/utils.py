301d7a7fb07a09bf3979912b6343e5ad6e5ce1ca
Mansi Pandey <shivanshipandey071@gmail.com>
2025-10-13 03:06:20 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -2,12 +2,48 @@ import os
 import sys
 from src.exception import CustomException
 import dill
+from sklearn.metrics import r2_score
+
+
 def save_object(file_path, obj):
+    """
+    Saves any Python object as a pickle file using dill.
+    """
     try:
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    """
+    Trains and evaluates multiple regression models.
+    Returns a dictionary with model names and their R2 scores on test data.
+    """
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model_name = list(models.keys())[i]
+
+            # Fit model
+            model.fit(X_train, y_train)
+
+            # Predictions
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            # R2 scores
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
     except Exception as e:
         raise CustomException(e, sys)
